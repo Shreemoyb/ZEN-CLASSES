7796a02ea5f473526eeebcbe1c012f1ae9482c6c
Shreemoy Banerjee <shreemoyb@gmail.com>
2023-02-10 23:58:46 +0530

    laud file DAY 12

--- Day 1 & 2/index.js
+++ Day 1 & 2/index.js
@@ -1,15 +1,16 @@
 const arr = [ 2, null, undefined, true, NaN, 'B'] ;
 
-for ( var element of arr) {
-    console.log(element);
-}
-
 for ( var i=0; i< arr.length; i++){
     console.log(arr[i]);
 }
 for ( var i in arr) {
     console.log(arr[i]);
 }
+
+for ( var element of arr) {
+    console.log(element);
+}
+
 arr.forEach((element)=> {
     console.log(element);
 })
---
+++ Day 12/test.js
@@ -0,0 +1,19 @@
+var geek = class geek {
+
+    constructor(a, b) {
+  
+      this.a = a;
+  
+      this.b = b;
+  
+    }
+  
+  };
+  
+  var guvi = new geek();
+  
+  guvi.a = 10;
+  
+  guvi.b = 10;
+  
+  console.log(guvi);
